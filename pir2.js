d97355b227bf411af01425fd189745a7eb90f83e
dougcoutts <doug.coutts@salishseadiving.com>
2018-05-01 15:16:52 -0700

    initial commit of pir prototype

---
+++ pir2.js
@@ -0,0 +1,22 @@
+var Gpio = require('onoff').Gpio,
+  sensor = new Gpio(17, 'in', 'both'),
+  led = new Gpio(4, 'out');    //#A
+
+sensor.watch(function (err, value) { //#B
+  if (err) exit(err);
+  console.log(value ? 'there is someone!' : 'not anymore!');
+  led.write(value, function() { //#E
+    console.log("Changed LED state to: " + value);
+  });
+});
+
+function exit(err) {
+  if (err) console.log('An error occurred: ' + err);
+  sensor.unexport();
+  console.log('Bye, bye!')
+  process.exit();
+}
+process.on('SIGINT', exit);
+
+// #A Initialize pin 17 in input mode, 'both' means we want to handle both rising and falling interrupt edges
+// #B Listen for state changes on pin 17, if a change is detected the anonymous callback function will be called with the new value
